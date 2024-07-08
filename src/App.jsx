de9882f2c22da74629a193396fd4ff33f5234150
박지훈 <pjh@ziumks.com>
2024-07-08 09:02:55 +0900

    주석제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BookList from "./components/book/BookList";
 import BookInsert from "./components/insert/BookInsert";
-import { useEffect, useState } from "react";
 import "./App.css";
 import { BookProvider } from "./components/contexts/BookContextAPI";
 
 function App() {
-  // const [addBookList, setAddBookList] = useState([]);
-
-  // const addBook = (book) => {
-  //   setAddBookList([...addBookList, book]);
-  // };
-
-  // useEffect(() => {
-  //   console.log("useEffect []");
-  //   const getBooks = JSON.parse(localStorage.getItem("books"));
-  //   setAddBookList(getBooks);
-  // }, []);
-
-  // useEffect(() => {
-  //   console.log("useEffect [addBookList]");
-  //   localStorage.setItem("books", JSON.stringify(addBookList));
-  // }, [addBookList]);
-
   return (
     <div className="app-container">
       <header className="app-header">읽은 책 목록</header>
